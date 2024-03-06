f7472ba4c8d56e08b1ceef57d86d312539c2133b
Fran Martin <alu0101316062@ull.edu.es>
2024-03-06 11:29:30 +0000

    modi temrinada

--- tests/adaptador.spec.ts
+++ tests/adaptador.spec.ts
@@ -3,48 +3,6 @@ import { expect } from 'chai';
 import { Adapter, Complex } from '../src/modi-pr7/adaptador';
 import { Rational } from '../src/modi-pr7/adaptador';
 
-// describe('Adapter', () => {
-//     describe('add', () => {
-//         it('debe sumar correctamente', () => {
-//             const rational1 = new Rational(2, 3);
-//             const rational2 = new Rational(3, 4);
-//             const adapter1 = new Adapter(rational1);
-//             const adapter2 = new Adapter(rational2);
-//             //const result = adapter1.add(adapter2);
-//             expect(adapter1.add(adapter2)).to.be.deep.equal(new Adapter(new Rational(5,7)));
-
-//             //expect(result.getReal()).to.equal(29 / 12);
-//            // expect(result).to.deep.equal(5/7)
-//            // expect(result.getImaginary()).to.equal(1);
-//         });
-//     });
-
-//     describe('subtract', () => {
-//         it('debe restar correctamente', () => {
-//             const rational1 = new Rational(2, 3);
-//             const rational2 = new Rational(3, 4);
-//             const adapter1 = new Adapter(rational1);
-//             const adapter2 = new Adapter(rational2);
-//             const result = adapter1.substract(adapter2);
-//             expect(result.getReal()).to.equal(-5 / 12);
-//             expect(result.getImaginary()).to.equal(1);
-//         });
-//     });
-
-//     describe('multiply', () => {
-//         it('debe multiplicar correctamente', () => {
-//             const rational1 = new Rational(2, 3);
-//             const rational2 = new Rational(3, 4);
-//             const adapter1 = new Adapter(rational1);
-//             const adapter2 = new Adapter(rational2);
-//             const result = adapter1.multiply(adapter2);
-//             expect(result.getReal()).to.equal(6/12);
-//             expect(result.getImaginary()).to.equal(1);
-//         })
-//     });
-
-// });
-
 describe('Adapter', () => {
     describe('constructor', () => {
       it('should correctly construct a complex number from a rational number', () => {
